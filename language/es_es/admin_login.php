8d11d473178dc02a9ce4964c198f770592a6a576
Luis Mendoza <mendozal@gmail.com>
2024-06-18 12:53:10 -0400

    More Spanish fixes

--- language/es_es/admin_login.php
+++ language/es_es/admin_login.php
@@ -10,15 +10,15 @@
  */
 
 $lang['AdminLogin.index.page_title'] = 'Conectarse';
-$lang['AdminLogin.index.title_adminarea'] = '%1$s | Área de Personal';
+$lang['AdminLogin.index.title_adminarea'] = '%1$s | Área del Personal';
 $lang['AdminLogin.index.field_username'] = 'Usuario';
 $lang['AdminLogin.index.field_password'] = 'Contraseña';
-$lang['AdminLogin.index.field_rememberme'] = 'Recordarme.';
+$lang['AdminLogin.index.field_rememberme'] = 'Recordarme';
 $lang['AdminLogin.index.field_loginsubmit'] = 'Ingresar';
 $lang['AdminLogin.index.link_resetpassword'] = 'Restablecer mi contraseña';
 $lang['AdminLogin.otp.page_title'] = 'Inicio de sesión OTP';
-$lang['AdminLogin.otp.title_adminarea'] = '%1$s | Área de Personal';
-$lang['AdminLogin.otp.field_password'] = 'Contraseña única';
+$lang['AdminLogin.otp.title_adminarea'] = '%1$s | Área del Personal';
+$lang['AdminLogin.otp.field_password'] = 'Código de un solo uso';
 $lang['AdminLogin.otp.field_loginsubmit'] = 'Ingresar';
 $lang['AdminLogin.otp.link_login'] = 'Cancelar, Ingresar';
 $lang['AdminLogin.reset.page_title'] = 'Restablecer contraseña';
@@ -44,12 +44,12 @@ $lang['AdminLogin.setup.field_username'] = 'Usuario';
 $lang['AdminLogin.setup.field_email'] = 'Dirección de email';
 $lang['AdminLogin.setup.field_password'] = 'Contraseña';
 $lang['AdminLogin.setup.field_confirm_password'] = 'Confirmar contraseña';
-$lang['AdminLogin.setup.field_enter_key_true'] = 'Tengo que introducir una clave de licencia';
+$lang['AdminLogin.setup.field_enter_key_true'] = 'Tengo una clave de licencia para introducir';
 $lang['AdminLogin.setup.field_enter_key_false'] = 'Quiero iniciar una prueba gratuita de 30 días';
-$lang['AdminLogin.setup.field_submit'] = 'Acabado';
+$lang['AdminLogin.setup.field_submit'] = 'Finalizar';
 $lang['AdminLogin.!error.unknown_user'] = 'No se reconoce ese nombre de usuario o no se puede restablecer la contraseña.';
 $lang['AdminLogin.!error.captcha.invalid'] = 'El captcha introducido no es válido. Por favor, inténtelo de nuevo.';
 $lang['AdminLogin.!success.reset_sent'] = 'Se ha enviado un email de confirmación a la dirección registrada.';
 $lang['AdminLogin.!info.reset_password'] = 'Introduzca su nombre de usuario a continuación y se le enviará por email un enlace para que pueda establecer una nueva contraseña.';
-$lang['AdminLogin.!info.otp'] = 'Se requiere autenticación de dos factores para este usuario. Introduzca su OTP (contraseña de un solo uso) a continuación.';
+$lang['AdminLogin.!info.otp'] = 'Se requiere autenticación de dos factores para este usuario. Introduzca su clave de un solo uso a continuación.';
 
